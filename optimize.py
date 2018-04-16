222817e8fab3f627252a965a57b951967e82df7e
Charlie J Keith <ckeith@advr-inc.com>
2018-04-16 09:39:29 -0600

    Fixed shifts for hill climbing and functionalized it.

--- optimize.py
+++ optimize.py
@@ -3,88 +3,64 @@
 # a beam profiling camera and a Gaussian mode.
 # Charlie J Keith 2018
 
-from matplotlib.pyplot import imread, imshow
+from matplotlib.pyplot import imread
 from mode_overlap_integral import curModeOverlap
 from numpy import pi, array
 
-
+def hillclimb(fun,p_init,p_shifts):
+    """ Maximize fun, starting at p_init, iteratively over the set of
+    parameter shifts in p_shifts
+    """
+    f_max = fun(*p_init)
+    p = p_init
+    while True:
+        print('p= ' + str(p))
+        f_new = 0
+        p_new = p
+        for i in range(p_shifts.shape[0]):
+            f_val = fun(*(p+p_shifts[i]))
+            if f_val > f_max:
+                f_new = f_val
+                p_new = p+p_shifts[i]
+        
+        if f_new > f_max:
+            f_max = f_new
+            p = p_new
+        else:
+            break
+    
+    return p
 
 if __name__ == '__main__':
-    image = imread('GAH1732-04_G09_M210.bmp')
+    image = imread('image3.bmp')
     mo_fun = curModeOverlap(image)
 
-    initial_guess = [170, 130, 20, 12, 0]
+    # center of image, fairly small, 0 degree rotation
+    initial_guess = [160, 128, 20, 10, 0]
     
-    # optimize center location
     p = initial_guess
-    mo_max = mo_fun(*p)
-    shifts = array([[0,1,0,0,0],
-                    [1,1,0,0,0],
-                    [1,0,0,0,0],
-                    [1,-1,0,0,0],
-                    [0,-1,0,0,0],
-                    [-1,-1,0,0,0],
-                    [-1,0,0,0,0],
-                    [-1,1,0,0,0]])
-    while True:
-        print('p= ' + str(p))
-        mo_new = 0
-        p_new = p
-        for i in range(shifts.shape[0]):
-            mo = mo_fun(*(p+shifts[i]))
-            if mo > mo_max:
-                mo_new = mo
-                p_new = p+shifts[i]
-        
-        if mo_new > mo_max:
-            mo_max = mo_new
-            p = p_new
-        else:
-            break
+    shifts = [array([[0,1,0,0,0],        # shift centers
+                     [1,1,0,0,0],
+                     [1,0,0,0,0],
+                     [1,-1,0,0,0],
+                     [0,-1,0,0,0],
+                     [-1,-1,0,0,0],
+                     [-1,0,0,0,0],
+                     [-1,1,0,0,0]]),
+              array([[0,0,0,0,pi/36],    # shift rotation angle
+                     [0,0,0,0,-pi/36]]),
+              array([[0,0,0,1,0],        # shift widths
+                     [0,0,1,1,0],
+                     [0,0,1,0,0],
+                     [0,0,1,-1,0],
+                     [0,0,0,-1,0],
+                     [0,0,-1,-1,0],
+                     [0,0,-1,0,0],
+                     [0,0,-1,1,0]])]
+    for i in range(len(shifts)):
+        p = hillclimb(mo_fun,p,shifts[i])
     
-    # optimize rotation
-    shifts = array([[0,0,0,0,pi/180],
-                    [0,0,0,0,-pi/180]])
-    while True:
-        print('p= ' + str(p))
-        mo_new = 0
-        p_new = p
-        for i in range(shifts.shape[0]):
-            mo = mo_fun(*(p+shifts[i]))
-            if mo > mo_max:
-                mo_new = mo
-                p_new = p+shifts[i]
-        
-        if mo_new > mo_max:
-            mo_max = mo_new
-            p = p_new
-        else:
-            break
-
-    # optimize width
-    shifts = array([[0,0,0,0,1],
-                    [0,0,0,1,1],
-                    [0,0,0,1,0],
-                    [0,0,0,1,-1],
-                    [0,0,0,0,-1],
-                    [0,0,0,-1,-1],
-                    [0,0,0,-1,0],
-                    [0,0,0,-1,1]])
-    while True:
-        print('p= ' + str(p))
-        mo_new = 0
-        p_new = p
-        for i in range(shifts.shape[0]):
-            mo = mo_fun(*(p+shifts[i]))
-            if mo > mo_max:
-                mo_new = mo
-                p_new = p+shifts[i]
-        
-        if mo_new > mo_max:
-            mo_max = mo_new
-            p = p_new
-        else:
-            break
+    mo_max = mo_fun(*p)
     
     print('\nFinal result:')
     print('Overlap Integral = ' + str(mo_max))
